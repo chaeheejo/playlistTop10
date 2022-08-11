ca7d86dfbe0c8eab3403055306be84282f1f7e6b
chaeheejo <60cogml@gmail.com>
2022-08-11 13:25:42 +0900

    compose (-ing)

--- app/src/main/java/com/example/playlisttop10/songregisteration/PlaylistFragment.kt
+++ app/src/main/java/com/example/playlisttop10/songregisteration/PlaylistFragment.kt
@@ -6,13 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.lifecycle.Observer
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.ComposeView
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.example.playlisttop10.R
 import com.example.playlisttop10.UserRepository
 import com.example.playlisttop10.databinding.FragmentPlaylistBinding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 class PlaylistFragment : Fragment() {
@@ -22,6 +32,7 @@ class PlaylistFragment : Fragment() {
 
     private lateinit var tv_title: TextView
     private lateinit var btn_register: ImageButton
+    private lateinit var cv_list: ComposeView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,18 +42,46 @@ class PlaylistFragment : Fragment() {
         _binding = FragmentPlaylistBinding.inflate(inflater, container, false)
         tv_title = binding.playlistTvTitle
         btn_register = binding.playlistBtnRegister
+        cv_list = binding.playlistCvList
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var name = UserRepository.currUser!!.name
+        val name = UserRepository.currUser!!.name
         tv_title.text = "$name's TOP10 List"
 
         btn_register.setOnClickListener{
             findNavController().navigate(R.id.action_playlistFragment_to_registerSongFragment)
         }
 
+        cv_list.setContent {
+            val scrollState = rememberScrollState()
+            Column (
+                Modifier
+                    .padding(12.dp)
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+            ){
+                element(title = "title")
+            }
+        }
+
     }
+
+    @Composable
+    fun element(title: String){
+        Card(
+            Modifier
+                .padding(12.dp)
+                .border(width = 2.dp, color = Color.Black)
+                .height(100.dp)
+        ) {
+            Box(contentAlignment = Alignment.Center){
+                Text(text = title)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/playlisttop10/songregisteration/PlaylistViewModel.kt
+++ app/src/main/java/com/example/playlisttop10/songregisteration/PlaylistViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.playlisttop10.songregisteration
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class PlaylistViewModel : ViewModel() {
